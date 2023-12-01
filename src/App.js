a8333a8bc5e856da9772626674c07cf699bb1414
SUJAY GOWDA <sujay.gowda@i4sight.net>
2023-12-01 12:30:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
 
+const initialState = {
+  question: [],
+
+  // loading, error, ready, active, 'finished
+  status: 'loading',
+};
+function reducer(state, action) {}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('http://localhost:8000/questions');
+      const data = await res.json();
+      console.log(data);
+    }
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
